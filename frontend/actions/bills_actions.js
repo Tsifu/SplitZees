1fafbf6b4a2dc6b4929a9a56332ff71fa5236079
Tsi Lung <tsi.lung@gmail.com>
2017-03-15 10:16:41 -0400

    Updated bills action creator to handle errors

--- frontend/actions/bills_actions.js
+++ frontend/actions/bills_actions.js
@@ -3,6 +3,9 @@ import * as SettleUtil from '../util/settle_api_util';
 
 export const RECEIVE_BILL = "RECEIVE_BILL";
 export const RECEIVE_ALL_BILLS = "RECEIVE_ALL_BILLS";
+export const RECEIVE_SB_ERRORS = "RECEIVE_SB_ERRORS";
+export const RECEIVE_SF_ERRORS = "RECEIVE_SF_ERRORS";
+export const RECEIVE_CB_ERRORS = "RECEIVE_CB_ERRORS";
 
 export const receiveBill = (bill) => ({
   type: RECEIVE_BILL,
@@ -25,18 +28,49 @@ export const receiveAllBills = (bills) => {
   });
 };
 
+export const receiveCBErrors = (errors) => {
+
+  return {
+    type: RECEIVE_CB_ERRORS,
+    errors
+  };
+};
+
+export const receiveSBErrors = (errors) => {
+
+  return {
+    type: RECEIVE_SB_ERRORS,
+    errors
+  };
+};
+
+export const receiveSFErrors = (errors) => {
+
+  return {
+    type: RECEIVE_SF_ERRORS,
+    errors
+  };
+};
+
+
 export const fetchBills = () => dispatch => {
   return BillsUtil.fetchBills().then(bills => dispatch(receiveAllBills(bills)));
 };
 
 export const createBill = (bill) => dispatch => {
-  return BillsUtil.createBill(bill).then(bill => dispatch(receiveBill(bill)));
+  return BillsUtil.createBill(bill)
+    .then(bill => dispatch(receiveBill(bill)),
+      err => dispatch(receiveCBErrors(err.responseJSON)));
 };
 
 export const settleBill = (bill) => dispatch => {
-  return SettleUtil.settleBill(bill).then(bills => dispatch(receiveAllBills(bills)));
+  return SettleUtil.settleBill(bill)
+    .then(bills => dispatch(receiveAllBills(bills)),
+      err => dispatch(receiveSBErrors(err.responseJSON)));
 };
 
 export const settleFriend = (friend) => dispatch => {
-  return SettleUtil.settleFriend(friend).then(bills => dispatch(receiveAllBills(bills)));
+  return SettleUtil.settleFriend(friend)
+    .then(bills => dispatch(receiveAllBills(bills)),
+      err => dispatch(receiveSFErrors(err.responseJSON)));
 };
